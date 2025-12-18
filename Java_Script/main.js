43b39fb8c57c501723d4f8dfcefea2dc3aeefe17
Shepherd <62826887+Shepherd-Liu@users.noreply.github.com>
2025-12-18 10:43:29 +0800

    Update main.js

--- Java_Script/main.js
+++ Java_Script/main.js
@@ -9,9 +9,9 @@ let z = x + y;
 console.log(z);
 console.log(x + '+' + y + '=' + z);
 /* --------------------------------- keyword -------------------------------- */
-const name11 = 'bclee';
-console.log(name11);
-const name11 = 'wewe';
-console.log(name11);
+// const name11 = 'bclee';
+// console.log(name11);
+// const name11 = 'wewe';
+// console.log(name11);
 
 const x_y_z$ = '111';
