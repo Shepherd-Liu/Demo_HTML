fdbfc153d500b087f47c3d1b1b87e1e72bcc23ea
Shepherd <62826887+Shepherd-Liu@users.noreply.github.com>
2025-12-18 14:39:50 +0800

    Update main.js

--- Java_Script/main.js
+++ Java_Script/main.js
@@ -13,5 +13,21 @@ console.log(x + '+' + y + '=' + z);
 // console.log(name11);
 // const name11 = 'wewe';
 // console.log(name11);
-
-const x_y_z$ = '111';
+/* -------------------------------------------------------------------------- */
+/*                           JavaScript Conditionals                          */
+/* -------------------------------------------------------------------------- */
+/* ----------------------------- JavaScript else ---------------------------- */
+if (hour < 18) {
+  greeting = 'Good day';
+} else {
+  greeting = 'Good evening';
+}
+/* -------------------------- The else if Statement ------------------------- */
+let age = 8;
+if (let < 10) {
+  greeting = 'Good morning';
+} else if (greeting < 20) {
+  greeting = 'Good day';
+} else {
+  greeting = 'Good evening';
+}
