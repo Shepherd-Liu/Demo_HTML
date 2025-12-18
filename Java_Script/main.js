be04d042185d32b1124381c0a9d08870a51efe64
Shepherd <62826887+Shepherd-Liu@users.noreply.github.com>
2025-12-18 10:15:20 +0800

    Update main.js

--- Java_Script/main.js
+++ Java_Script/main.js
@@ -2,8 +2,9 @@
 function showtime() {
   "document.getElementById('demo').innerHTML = Date()";
 }
-
+/* ------------------------------- console.log ------------------------------ */
 let x = 7;
 let y = 10;
 let z = x + y;
 console.log(z);
+console.log(x + '+' + y + '=' + z);
