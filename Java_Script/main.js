bccc42e214572064c57bcf1ccd5a3db7f0ccedea
Shepherd <62826887+Shepherd-Liu@users.noreply.github.com>
2025-12-18 10:11:27 +0800

    Create main.js

---
+++ Java_Script/main.js
@@ -0,0 +1,9 @@
+/* -------------------------------- showtime -------------------------------- */
+function showtime() {
+  "document.getElementById('demo').innerHTML = Date()";
+}
+
+let x = 7;
+let y = 10;
+let z = x + y;
+console.log(z);
