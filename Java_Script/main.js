d7c672cacffffc6183c56bd0d3017f6b6e54ad97
Shepherd <62826887+Shepherd-Liu@users.noreply.github.com>
2025-12-18 10:32:30 +0800

    Update main.js

--- Java_Script/main.js
+++ Java_Script/main.js
@@ -8,3 +8,8 @@ let y = 10;
 let z = x + y;
 console.log(z);
 console.log(x + '+' + y + '=' + z);
+/* --------------------------------- keyword -------------------------------- */
+let x;
+x = 5;
+let y = 6;
+console.log(y);
